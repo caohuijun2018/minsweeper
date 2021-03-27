babf32dc92f7f488e14374b2d2c9662839c8294c
caohuijun2018 <1160364324@qq.com>
2021-03-27 19:27:52 +0800

    添加右击函数

--- src/component/Cell.js
+++ src/component/Cell.js
@@ -1,39 +1,36 @@
 import React from "react";
 
-const Cell = ({data,onClick}) => {
-  
-//  console.log('cell data:',data)
+const Cell = ({ data, onClick, contextMenu }) => {
+  //  console.log('cell data:',data)
+
   let className =
     "cell" +
-    (data.isRevealed  === true ? " " : " hidden") +
+    (data.isRevealed === true ? " " : " hidden") +
     (data.isMine === true ? " is-mine" : " ") +
     (data.isFlaged === true ? " is-flag" : " ");
 
-
-
   const cellContent = () => {
-    console.log('cellcontent')
-    if (!data.isRevealed ) {  //isRevealed为false时执行
-      if(data.isFlaged) {
-        return '🚩'
+    // console.log("cellcontent");
+    if (!data.isRevealed) {
+      //isRevealed为false时执行
+      if (data.isFlaged) {
+        return "🚩";
       }
       // if(data.isMine) return '❌'  //debug时用于标记地雷位置
-    }else {  //点击之后，如果为地雷，则显示地雷。如果为空则显示空，如果有neighbour则显示数字
+    } else {
+      //点击之后，如果为地雷，则显示地雷。如果为空则显示空，如果有neighbour则显示数字
       if (data.isMine) {
-        return '💣';
+        return "💣";
       }
       if (data.neighbour !== 0) {
-        console.log(data.neighbour)
-        return '1';
-      } 
-        return ' ';
+        return "1";
+      }
+      return " ";
     }
-    
-    
   };
   // console.log(className)
   return (
-    <div className={className} onClick={onClick}>
+    <div className={className} onClick={onClick} contextMenu={contextMenu}>
       {cellContent()}
     </div>
   );
